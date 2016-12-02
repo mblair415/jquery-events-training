2b9c3addf46d81bef2da56d20433e580aba68818
mblair415 <mblair415@gmail.com>
2016-12-01 20:42:00 -0800

    click and change complete

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('.add').change(function() {
+    var a = $('#left').val();
+    var b = $('#right').val();
+    var c = $('#total').val(Number(a)+Number(b));
+  })
+
+$(selector).change()
+
+$('body').append("<button type="button">Reset</button>"
+
+
 })
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,20 @@
 console.log("Sanity Check: JS is working!");
 
+// $(document).ready(function(){
+//
+//   $('span').on('click', function handleClick(even) {
+//     var buttonText = $(this).text() +
+//       $('ul').append('<li>' + buttonText + '</li>')
+//   })
+//
+// })
+
+
 $(document).ready(function(){
+
+  $("span").on("click", function handleClick(event){
+    var buttonText = $(this).text() + " Clicked at: " + Date.now();
+    $("ul").append("<li>" + buttonText + "</li>")
+  })
 
 })
