d283a4a953d28aacfa7763939a572578b70c0297
mblair415 <mblair415@gmail.com>
2016-12-02 08:46:50 -0800

    updates for keypress and base.  updated keypress index trying to get it to see jquery

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -4,50 +4,59 @@ console.log("Sanity Check: JS is working!");
 The solution doesn't work.  I don't know how to use setTimeOut.  It's 11:40
 I woke up at 6:00am and all I've done today is code and travel to places
 to do more coding.
+started on this again on the ferry.
 */
 
 $(document).ready(function(){
+  var totalTime = 0;
+  var counting = false;
 
+  $(window).on("keypress", function handleKeypress(event){
+    if (!counting) {
+      counting = true;
+      timer();
+    } else {
+      counting = false;
+    }
 
+    function getSeconds(){
+      var seconds = totalTime % 60;
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      return seconds;
+    }
+
+    function getMinutes(){
+      var minutes = (totalTime/60) % 60;
+
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
+      return minutes;
+    }
+
+    function getHours() {
+      var hours = ((totalTime/60)/60) % 60;
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+    }
+
+    function printTime() {
+      return getHours() + ":" + getMinutes() + ":" + getSeconds();
+    }
+
+    function timer() {
+      setInterval(function() {
+        totalTime += 1;
+        $("#total-time").text(printTime());
+      }, 1000)
+    }
+
+  })
 })
-var totalTime = 0;
-
-function getSeconds(){
-  var seconds = totalTime % 60;
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  return seconds;
-}
-
-function getMinutes(){
-  var minutes = (totalTime/60) % 60;
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  return minutes;
-}
-
-function getHours() {
-  var hours = ((totalTime/60)/60) % 60;
-
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-}
-
-function printTime() {
-  console.log(getHours() + ":" + getMinutes() + ":" + getSeconds());
-}
-
-function timer() {
-  setTimeOut(function() {
-    totalTime += 1;
-  }, 1000)
-}
-
-timer();
--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -1,14 +1,14 @@
 console.log("Sanity Check: JS is working!");
 
-var rick_roll_youtube_embed_code = "<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allowfullscreen></iframe>";
+var rickRolled = "<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allowfullscreen></iframe>";
 
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
   $("form").on("submit", function handleClick(event){
-      event.preventDefault();
-
+    event.preventDefault();
+    ("body").append(rickRolled);
   });
 
 })
